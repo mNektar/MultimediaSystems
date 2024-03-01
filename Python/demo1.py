85c4f4391b00fe328c5bd8e0930719fcd98e1c1a
Nektarios Mastoras <57330052+mNektar@users.noreply.github.com>
2024-03-01 19:40:17 +0200

    Update demo1.py

--- Python/demo1.py
+++ Python/demo1.py
@@ -4,9 +4,13 @@ import matplotlib.image as mpimg
 import PreProcessing
 import TransformDCT
 import Quantization
-
+# Adjust image directory
 imageRGB_baboon = mpimg.imread('D:/Documents/University/9/Multimedia/Project/baboon.png')
 imageRGB_lena = mpimg.imread('D:/Documents/University/9/Multimedia/Project/lena_color_512.png')
+# Change the range of the images from [0, 1] to [0, 255]
+# If range is not changed quantization cannot work since it will round everything at either 0 or 1
+imageRGB_baboon = (imageRGB_baboon * 255).astype(int)
+imageRGB_lena = (imageRGB_lena * 255).astype(int)
 
 def part1(image, subimg):
     imageRGB = PreProcessing.crop_if_needed(image)
@@ -48,44 +52,43 @@ def part2(image, subimg, qScale):
     QuantizedY = np.zeros_like(imageY)
     QuantizedCr = np.zeros_like(imageCr)
     QuantizedCb = np.zeros_like(imageCb)
+    deQuantizedY = np.zeros_like(imageY)
+    deQuantizedCr = np.zeros_like(imageCr)
+    deQuantizedCb = np.zeros_like(imageCb)
     for i in range(0, imageY.shape[0], 8):
         for j in range(0, imageY.shape[1], 8):
             blockY = imageY[i:i + 8, j:j + 8]
             dctBlockY[i:i + 8, j:j + 8] = TransformDCT.blockDCT(blockY)
             QuantizedY[i:i + 8, j:j + 8] = Quantization.quantizeJPEG(dctBlockY[i:i + 8, j:j + 8], qTableLuminance, qScale)
-            imageY[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(QuantizedY[i:i + 8, j:j + 8])
+            deQuantizedY[i:i + 8, j:j + 8] = Quantization.dequantizeJPEG(QuantizedY[i:i + 8, j:j + 8], qTableLuminance, qScale)
+            imageY[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(deQuantizedY[i:i + 8, j:j + 8])
     for i in range(0, imageCr.shape[0], 8):
         for j in range(0, imageCr.shape[1], 8):
             blockCr = imageCr[i:i + 8, j:j + 8]
             dctBlockCr[i:i + 8, j:j + 8] = TransformDCT.blockDCT(blockCr)
             QuantizedCr[i:i + 8, j:j + 8] = Quantization.quantizeJPEG(dctBlockCr[i:i + 8, j:j + 8], qTableChrominance, qScale)
-            imageCr[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(QuantizedCr[i:i + 8, j:j + 8])
+            deQuantizedCr[i:i + 8, j:j + 8] = Quantization.dequantizeJPEG(QuantizedCr[i:i + 8, j:j + 8], qTableLuminance, qScale)
+            imageCr[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(deQuantizedCr[i:i + 8, j:j + 8])
     for i in range(0, imageCb.shape[0], 8):
         for j in range(0, imageCb.shape[1], 8):
             blockCb = imageCb[i:i + 8, j:j + 8]
             dctBlockCb[i:i + 8, j:j + 8] = TransformDCT.blockDCT(blockCb)
             QuantizedCb[i:i + 8, j:j + 8] = Quantization.quantizeJPEG(dctBlockCb[i:i + 8, j:j + 8], qTableChrominance, qScale)
-            imageCb[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(QuantizedCb[i:i + 8, j:j + 8])
+            deQuantizedCb[i:i + 8, j:j + 8] = Quantization.dequantizeJPEG(QuantizedCb[i:i + 8, j:j + 8], qTableLuminance, qScale)
+            imageCb[i:i + 8, j:j + 8] = TransformDCT.iBlockDCT(deQuantizedCb[i:i + 8, j:j + 8])
     reconstructed_imageRGB = PreProcessing.convert2rgb(imageY, imageCr, imageCb, subimg)
     # Plotting both images
     plt.figure(figsize=(15, 5))
-    plt.subplot(1, 3, 1)
+    plt.subplot(1, 2, 1)
     plt.imshow(imageRGB)
     plt.title('RGB')
-    plt.subplot(1, 3, 2)
+    plt.subplot(1, 2, 2)
     plt.imshow(reconstructed_imageRGB)
     plt.title('Reconstructed RGB')
-    # Calculating and plotting the difference
-    image_diff = np.clip((imageRGB - reconstructed_imageRGB), 0, 255)
-    plt.subplot(1, 3, 3)
-    plt.imshow(image_diff)
-    plt.title('Difference')
-    plt.tight_layout()
     plt.show()
     return
 
-# part1(imageRGB_baboon, [4, 2, 0])
-# part1(imageRGB_lena, [4, 4, 4])
+part1(imageRGB_baboon, [4, 2, 0])
+part1(imageRGB_lena, [4, 4, 4])
 part2(imageRGB_baboon, [4, 2, 2], 0.6)
-# part2(imageRGB_lena, [4, 4, 4], 5)
-
+part2(imageRGB_lena, [4, 4, 4], 5)
